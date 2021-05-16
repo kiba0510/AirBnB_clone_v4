0a50d7cd0f665556163f8e16a8654c4f09a83ddc
AndresSern <andrecamposer@gmail.com>
2021-05-16 16:33:39 -0500

    Modifying all files and adding the file 3-hbnb.py and 3-hbnb.js and 3-hbnb.html

--- web_dynamic/static/script/2-hbnb.js
+++ web_dynamic/static/script/2-hbnb.js
@@ -18,12 +18,12 @@ function init () {
 }
 
 function api_Status () {
-    const API_URL = `http://${HOST}:5001/api/v1/status/`;
-    $.get(API_URL, (data, textStatus) => {
-      if (textStatus === 'success' && data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    });
-  }
+  const API_URL = `http://${HOST}:5001/api/v1/status/`;
+  $.get(API_URL, (data, textStatus) => {
+    if (textStatus === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+}
